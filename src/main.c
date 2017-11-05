41a8bcf914cb956f85671bad7166b4e174f4c251
Brendan Horng <brendan.horng@ludusamo.com>
2017-11-04 21:31:33 -0400

    Add basic outline for repl

--- src/main.c
+++ src/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <hashtable.h>
 
-int main() {
-	Hashtable h;
-	ctor_hashtable(&h);
-	set_hashtable(&h, "Potato", from_double(34));
-	int i = access_hashtable(&h, "Potato").as_int32;
-	printf("This should be 34: %d", i);
+void run(const char *exp) {
+    printf("I am going to run: %s\n", exp);
 }
+
+void run_script(char *file_path) {
+    return;
+}
+
+void run_repl() {
+    char exp[2048];
+    for (;;) {
+        printf("> ");
+        scanf("%s", exp);
+        run(exp);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        puts("Usage: angstrom [script]");
+    } else if (argc == 2) {
+        run_script(argv[1]);
+    } else {
+        run_repl();
+    }
+
+    return 0;
+}
+
